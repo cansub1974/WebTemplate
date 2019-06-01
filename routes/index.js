854217dbb772c528148138b5c1f8ef6b25a4b3a7
cansub1974 <jasonalexandermckay@hotmail.com>
2019-06-01 13:33:29 -0400

    Updates

--- routes/index.js
+++ routes/index.js
@@ -12,22 +12,18 @@ var User = require('../models/user');
 router.get('/', function (req, res, next) {
     var successMsg = req.flash('success')[0];
     Product.find(function (err, docs) {
-        var productChunks = [];
-        var chunkSize = 3;
-        for (var i = 0; i < docs.length; i += chunkSize) {
-            productChunks.push(docs.slice(i, i + chunkSize));
-        }
-        res.render('shop/index', {
-            title: 'Shopping Cart',
-            products: productChunks,
-            successMsg: successMsg,
-            noMessages: !successMsg,
-            helpers: {
-                foo: function () {
-                    return 'foo.';
-                }
+            var productChunks = [];
+            var chunkSize = 3;
+            for (var i = 0; i < docs.length; i += chunkSize) {
+                productChunks.push(docs.slice(i, i + chunkSize));
             }
-        });
+            res.render('shop/index', {
+                    title: 'Shopping Cart',
+                    products: productChunks,
+                    successMsg: successMsg,
+                    noMessages: !successMsg,
+                }
+            });
     });
 });
 
